0a339fe591971af1ab3bc27bec75551f6ebfc368
shjoonalameer <shjoonsultan@hotmail.com>
2025-02-14 14:01:34 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/my-portfolio", // Must match your GitHub repo name
+  assetPrefix: "/my-portfolio/", // Ensures CSS and assets load correctly
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
